078ec1c0f381f87f11db5ecdd54662d5e206e2b9
gsmalves <gustavoeu2017@gmail.com>
2022-06-07 18:47:13 -0300

    add novo arquivo netlist para teste de circuito maior, com 4 nós

--- AnaliseNodal.cpp
+++ AnaliseNodal.cpp
@@ -104,7 +104,7 @@ int main(void)
 {
   system("cls");
   ne=0; nv=0; strcpy(lista[0],"0");
-  arquivo=fopen("NetlistSimples.net","r");
+  arquivo=fopen("NetlistSimplesII.net","r");
 
   printf("Lendo netlist:\n");
   fgets(txt,MAX_LINHA,arquivo);
